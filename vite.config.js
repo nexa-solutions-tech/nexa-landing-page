324854d8dca7ecc21398461e577298034ee2c45e
Marcus <marcusvini678@gmail.com>
2026-02-28 12:42:52 -0300

    feat: faz ajuste

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
   plugins: [react()],
   publicDir: "./static",
-  base: "/nexa-landing-page/",
+  base: "/",
   css: { postcss: { plugins: [tailwind()] } },
   resolve: { alias: { "@": path.resolve(__dirname, "src") } },
 });
